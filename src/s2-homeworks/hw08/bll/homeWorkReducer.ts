7019a780099ac4bca218d7cd7d4a31bfbdc54d59
Dmitry-Klyuev <i6169696@gmail.com>
2024-11-25 23:14:55 +0300

    hw8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,17 +4,24 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): any => { // need to fix any
     switch (action.type) {
-        case 'sort': { // by name
-
-            return state // need to fix
+        case 'sort': {
+            return [...state.sort((a, b) => action.payload === 'up' ? a.name > b.name ? 1 : -1 : a.name < b.name ? 1 : -1)]
         }
         case 'check': {
 
-            return state // need to fix
+            return [...state.filter(user => user.age > action.payload)] // need to fix
         }
         default:
             return state
     }
+}
+
+export const sort = (payload: {up: string}) => {
+    return {type: 'sort', payload} as const
+}
+
+export const check = (payload: {age: number }) => {
+    return {type: 'check', payload} as const
 }
